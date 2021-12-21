bdcf5a2b1bde40d5704afbd354a0e07f7eedbd26
Tomer <tomer.kamar@gmail.com>
2021-12-21 20:26:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hobbieshare/Activities/activity_main.java
@@ -0,0 +1,18 @@
+package com.example.hobbieshare.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hobbieshare.R;
+
+public class activity_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/hobbieshare/Activities/activity_opening_screen.java
@@ -0,0 +1,88 @@
+package com.example.hobbieshare.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
+
+import com.example.hobbieshare.R;
+
+public class activity_opening_screen extends AppCompatActivity {
+
+    private ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+
+        findViews();
+        logo.setVisibility(View.INVISIBLE);
+        startAnimation(logo);
+
+    }
+
+    private void startAnimation(ImageView img) {
+        img.setVisibility(View.VISIBLE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        img.setY(-height / 2);
+        img.setScaleY(0.0f);
+        img.setScaleX(0.0f);
+        img.animate()
+                .scaleY(1.0f)
+                .scaleX(1.0f)
+                .translationY(0)
+                .setDuration(3000)
+                .setInterpolator(new AccelerateInterpolator())
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        animationDone();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+    }
+
+    private void animationDone() {
+        openHomeActivity();
+    }
+
+    private void openHomeActivity() {
+        Intent intent = new Intent(this, activity_main.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        startActivity(intent);
+
+    }
+
+    private void findViews() {
+        logo = findViewById(R.id.actibity_opening_screen_logo);
+    }
+}
